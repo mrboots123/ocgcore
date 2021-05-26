32931eea633af8c75961a579ec3fbd7314a3a34a
Diego <dvazque7@asu.edu>
2021-05-26 14:40:18 -0700

    bringing in cards db

--- core/index.js
+++ core/index.js
@@ -1,10 +1,44 @@
+const HashMap = require('hashmap');
+
 const Lib = (function () {
     let instance;
-
+    const _datas = new HashMap();
+    const _strings = new HashMap();
 
     function createInstance() {
 
-        return null;
+        const db = require('better-sqlite3')('/Users/user/Desktop/ocgcore/core/bin/cards.cdb', {});
+
+        const results = db.prepare('select * from datas,texts where datas.id=texts.id').all();
+
+        results.forEach(result => {
+            const {
+                id, ot, alias, setcode, type, atk, def, level, race,
+                attribute, category, name, desc,
+            } = result;
+
+            _datas.set(id, {
+                id,
+                ot,
+                alias,
+                setcode,
+                type,
+                atk,
+                def,
+                level: level & 0xff,
+                lscale:  (level >> 24) & 0xff,
+                rscale:  (level >> 16) & 0xff,
+                race,
+                attribute,
+                category,
+            });
+            _strings.set(id, {
+                name,
+                desc,
+            });
+        });
+
+        return 'null';
     }
 
     function getInstance() {
@@ -14,8 +48,16 @@ const Lib = (function () {
         return instance;
     }
 
+    function getData() {
+        return _datas;
+    }
+    function getStrings() {
+        return _strings;
+    }
     return {
         getInstance,
+        getData,
+        getStrings
     };
 })();
 
--- main.js
+++ main.js
@@ -1,15 +1,50 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import Singleton from "./store";
-import {msg_draw, msg_new_turn, msg_select_idlecmd} from "./store/action";
-import Socket from "./socket";
+// import Lib from "./core";
+//
+// const sqlite3 = require('sqlite3')
+// const { open } = require('sqlite')
+// const HashMap = require('hashmap');
+//
+// open({
+//     filename: './cards.cdb',
+//     driver: sqlite3.Database
+// }).then(async (db) => {
+//     const results = await db.all('select * from datas,texts where datas.id=texts.id');
+//     const _datas = new HashMap();
+//     const _strings = new HashMap();
+//
+//     results.forEach(result => {
+//         const {
+//             id, ot, alias, setcode, type, atk, def, level, race,
+//             attribute, category, name, desc,
+//         } = result;
+//         _datas.set(id, {
+//             id,
+//             ot,
+//             alias,
+//             setcode,
+//             type,
+//             atk,
+//             def,
+//             level: level & 0xff,
+//             lscale:  (level >> 24) & 0xff,
+//             rscale:  (level >> 16) & 0xff,
+//             race,
+//             attribute,
+//             category,
+//         });
+//         _strings.set(id, {
+//             name,
+//             desc,
+//         });
+//     });
+//
+//     const ocgcore = Lib.getInstance();
+//
+//
+// });
 
+import Lib from "./core";
 
-const socket = Socket.getInstance();
+const lib = Lib.getInstance();
 
-
-
-//store.dispatch(msg_draw({ player: 0, hand: [], }))
-
-
-
+console.log(Lib.getData())
